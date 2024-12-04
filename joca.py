67ecf4ef5d3703e6964d9af171a938ceded738ae
Thiago Prado <thiagoaoap@al.insper.edu.br>
2024-12-03 21:03:40 -0300

    deploy

--- joca.py
+++ joca.py
@@ -1,1 +1,12 @@
-print('joca gay')
+from flask import render_template, Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def pagina_home():
+    return render_template('joca.html')
+
+
+if __name__ == "__main__":
+    app.run(port=8000, debug=True)
